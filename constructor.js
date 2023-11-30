9ecd0160a5496c327f2a88265f759f43a479bace
Nitika- Thakur123 <thakurnitika474@gmail.com>
2023-11-30 10:19:19 +0530

    bank

--- constructor.js
+++ constructor.js
@@ -1,11 +1,41 @@
 
 
+    
+    function CreateBankAccount(accountNumber,name,type,balance){
 
-function CreateBankAccount(accountNumber,name,type,balance){
-
-    this.accountNumber=accountNumber;
-    this.name=name;
-    this .type=type;
-    this.balance=balance;
-
-    }
+        this.accountNumber=accountNumber;
+        this.name=name;
+        this .type=type;
+        this.balance=balance;
+        
+        this.deposite=function(amount){
+           
+        
+            this.balance +=amount;
+        
+           }
+            this.withDraw = function(amount){
+                if(amount <= this.balance) {
+                    this.balance -= amount/
+                    console.log("succesfully",amount)
+                }else{
+                    console.log("hey you have insuficient balance" ,this.balance)
+                }
+        
+                // this.balance -= amount
+            }
+            this.getTotalBalance = function(){
+                console.log('your total balance is this- ',this.balance)
+            }
+        
+        };
+        let ac1 = new CreateBankAccount(1234,"Nitika","savings",500)
+        // console.log(ac1,"without deposite")
+        // ac1.deposite(100)
+        // console.log(ac1)
+        ac1.withDraw(300)
+        ac1.getTotalBalance()
+        // console.log(ac1)
+        
+        // console.log(CreateBankAccount())
+    
